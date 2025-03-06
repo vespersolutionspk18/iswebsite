b9cbb0306bf74dca5744245ebcb5d901c1a70ca1
Mac <vespersolutionspk@gmail.com>
2025-03-06 14:21:58 +0500

    first committttd

--- .history/app/ClientCarousel_20250304214306.tsx
+++ .history/app/ClientCarousel_20250304214306.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 import Image from 'next/image';
-//test
+
 const ClientCarousel = () => {
   const logoSize = { height: 40 };
   const logoStyle = { margin: '0 25px' };
